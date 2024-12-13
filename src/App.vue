b2c811220235e97a0c0e0c990368a80fdfbc0382
HebrewPhilology <frederique.rey@univ-lorraine.fr>
2024-12-13 01:04:39 +0100

    add themes and adjust app-bar

--- src/App.vue
+++ src/App.vue
@@ -3,57 +3,62 @@
   <div>
     <!-- Première app-bar avec le logo-->
     <v-app-bar
-    color="primary" 
-    density="prominent" flat
-    > <!-- or, instead of prominent, add style="padding: 10px 0;" -->
-
-    <!-- Toogle pour le choix du thème -->
-      <!-- <v-btn @click="toggleTheme">Toggle Theme</v-btn> -->
+    color="primary"
+    density="prominent"
+    flat
+    >
 
     <!-- Menu déroulant choix thème -->
-    <v-select
-          v-model="selectedTheme"
-          :items="myThemes"
-          label="Select Theme"
-          @update:modelValue="setTheme()"
-        />
+    <!--
+    <v-row class="d-flex">
+      <v-col>
+        <v-select class="rounded" style="width: 200px;"
+              v-model="selectedTheme"
+              :items="myThemes"
+              @update:modelValue="setTheme()"
+            />
+      </v-col>
+    </v-row>
+    -->
 
+      <!-- logo -->
+      <v-row>
+        <v-col class="ma-6">
+          <router-link to="/">
+            <v-img class="mx-auto"
+            src="/src/assets/Logo.png" alt="Logo"  style="width: 300px;"/>
+          </router-link>
+        </v-col>
+      </v-row>
 
-    <!-- logo -->
-    <v-container class="d-flex justify-center align-center" style="height: 100%;">
-              <img src="@/assets/logo.png" alt="Logo" style="width: 300px;" />
-      </v-container>
     </v-app-bar>
 
     
     <!-- Deuxième app-bar avec le menu-->
     <v-app-bar color="primary-darken-1">
 
-            <!-- Menu burger -->
-            <v-app-bar-nav-icon  class="d-md-none" @click="drawer = !drawer" />
+      <!-- Menu burger -->
+      <v-app-bar-nav-icon  class="d-md-none" @click="drawer = !drawer" />
 
-            <!-- Bouton Home -->
-             <v-app-bar-title>
-            <v-btn  flat to="/">
-              <v-icon left dark class="mx-3">mdi-home</v-icon>
-              Home
-            </v-btn>
-          </v-app-bar-title>
-    
-          <!-- À droite : Menus -->
-            <v-btn
-              
-              flat
-              v-for="item in menuItems"
-              :key="item.title"
-              :to="item.path"
-              class="d-none d-md-flex"
-            >
-              <v-icon left dark class="mx-3">{{ item.icon }}</v-icon>
-              <span class="d-none d-lg-flex">
-              {{ item.title }}
-              </span>
-            </v-btn>
+      <!-- Bouton Home -->
+      <v-app-bar-title>
+        <!-- <v-btn  flat to="/">
+          <v-icon left dark class="mx-3">mdi-home</v-icon>
+          Home
+        </v-btn> -->
+      </v-app-bar-title> 
+
+      <!-- À droite : Menus -->
+      <v-btn
+        flat
+        v-for="item in menuItems"
+        :key="item.title"
+        :to="item.path"
+        class="d-none d-md-flex"
+        >
+        <v-icon left dark class="mx-3">{{ item.icon }}</v-icon>
+        <span class="d-none d-lg-flex">{{ item.title }}</span>
+      </v-btn>
     </v-app-bar>
 
     <!-- Drawer pour le menu burger -->
@@ -98,7 +103,7 @@ import { ref } from "vue"
 import { useTheme } from "vuetify"
 
 const theme = useTheme();
-const myThemes = ["light", "dark", "custom_theme"];
+const myThemes = ["light", "uniquely_balanced", "deliciousness", "barbie", "taupe_beige"];
 const selectedTheme = ref(myThemes[0]);
 
 const setTheme = () => {
@@ -126,6 +131,7 @@ export default {
       //currentTheme: ref(this.$vuetify.theme.global.name),
       drawer: false, // Contrôle de l'état du menu burger
       menuItems: [
+        { title: 'Home', path: '/', icon: 'mdi-home'},
         { title: 'History', path: '/history', icon: 'mdi-history' },
         { title: 'Board', path: '/board', icon: 'mdi-account-group' },
         { title: 'Issues', path: '/issues', icon: 'mdi-table-of-contents' },
@@ -147,4 +153,11 @@ export default {
 .bg {
   background: url('./assets/background_white_pattern.jpg');
 }
+
 </style>  
+
+<style>
+.v-field {
+    max-width: 200px;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ const vuetify = createVuetify({
   components,
   directives,
   theme: {
-    defaultTheme: 'light',
+    defaultTheme: 'uniquely_balanced',
     themes: {
       light: {
         colors: {
@@ -33,14 +33,41 @@ const vuetify = createVuetify({
       },
       dark: {
         colors: {
-          //Uniquely Balanced Pantone 2025
-          primary: '#ca80b1', 
-          'primary-darken-1': '#9c4c8d', 
-          secondary: '##d0baba', 
-          'secondary-darken-1': '#a37864',
+          //Blue nova https://www.benjaminmoore.com/en-us/paint-colors/color-of-the-year-2024
+          primary: '#5b6d92', // blue nova 825
+          'primary-darken-1': 'rgb(54, 64, 66)', //regent Green
+          secondary: '#9b8e9c', //Hazy Lilac
+          'secondary-darken-1': 'rgb(140, 138, 123)', //Antique Pewter
+          'secondary-darken-2': '#cbdde2', //Polar Sky
         },
       },
-      Barbie: {
+      uniquely_balanced: {
+        colors: {
+          //Uniquely Balanced Pantone 2025
+          primary: '#9c4c8d', //Pantone 18-3223 TCX Cattleya Orchid
+          'primary-darken-1': '#ca80b1', //Pantone 16-3116 TCX Opera Mauve
+          //secondary: '#F0EFEE', //Arrière planc blanc cassé pour texte
+          'secondary-darken-1': '#D16F52', // PANTONE™ 16-1441 TCX - Arabesque
+          'secondary-darken-2': '#d8ae48', // PANTONE* 14-0952 TCX - Spicy Mustard
+          'secondary-darken-3': '#33Becc', // PANTONE™ 15-4825 TCX - Blue Curacao
+          'secondary-darken-4': '#a4786a', //PANTONE* 17-1230 TCX - Mocha Mousse
+          secondary: '##FF968D', //PANTONE* 15-1435 TCX - Desert Flower
+        },
+      },
+      deliciousness: {
+        colors:{
+          //Deliciousness
+          secondary: 'rgb(241, 166, 204)', // PANTONE™ 15-2213 TCX - Bonbon
+          primary: 'rgb(197, 73, 102)', // PANTONE™ 17-1741 TCX - Party Punch
+          'primary-darken-1': 'rgb(126, 33, 42)', // PANTONE™ 19-1537 TCX - Winery
+          'secondary-darken-1': 'rgb(236, 106, 55)', // PANTONE™ 16-1459 TCX - Mandarin Orange
+          'secondary-darken-2': 'rgb(238, 109, 138)', // PANTONE* 16-1735 TCX - Pink Lemonade
+          'secondary-darken-4': 'rgb(164, 120, 100)', //PANTONE™ 17-1230 TCX - Mocha Mousse
+          'secondary-darken-5': 'rgb(195, 124, 84)', //PANTONE™ 16-1439 TCX - Caramel
+          //secondary: '#ffb18178', //PANTONE™ 14-1231 TCX - Peach Cobbler rgb(255, 177, 129) avec transparence
+          },
+        },
+      barbie: {
         colors: {
           primary: 'rgb(218, 26, 132)', // Barbie Pink
           'primary-darken-1': 'rgb(181, 152, 164)', //Mauve Shadows
@@ -48,7 +75,7 @@ const vuetify = createVuetify({
           'secondary-darken-1': 'rgb(247, 224, 232)',
         },
       },
-      Taupe_beige: {
+      taupe_beige: {
         colors: {
           primary: '#8a737d',
           'primary-darken-1': '#d2b1a2',
@@ -66,6 +93,12 @@ const app = createApp(App)
 app.use(router)
 app.use(vuetify)
 app.mount('#app')
+
+
+
+
+
+
 
 
 
